c8fab4daeb6b8e22b0e260ef20654c5f4ccf6f0c
Turtle G <goliag8@gmail.com>
2025-12-08 04:41:32 -0500

    Reapply "Add project files."
    This reverts commit c987f4140d0259de14731737189e2aa2ec3c2f46.

---
+++ T7CompilerGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("T7 Compiler GUI")]
+[assembly: AssemblyDescription("GUI Wrapper for T7 GSC Compiler")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T7CompilerGUI")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8b3c4d5-e6f7-4a8b-9c0d-1e2f3a4b5c6d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
